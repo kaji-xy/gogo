2faa4badd6cb0de3c3eb90e4d056eacc807789d6
Bikash Kaji <kaji@cloudfactory.com>
2024-05-24 12:16:02 +0100

    Toli package with toli information

---
+++ toli.go
@@ -0,0 +1,12 @@
+// Package toli offers information about our toli
+package toli
+
+// Title returns the title of this book.
+func title() string {
+	return "GoGo Toli: " + subtitle()
+}
+
+func subtitle() string {
+	return "Tolders on skates"
+}
+
